3c96eca6931051c5e1c56610e2d21f0a1e146823
Cristian Alejandro Cisneros Villegas <125778068+AlejandroCisnerosVillegas@users.noreply.github.com>
2024-06-11 14:20:33 -0600

    Add files via upload

--- ejercicio.php
+++ ejercicio.php
@@ -1,15 +1,11 @@
 <?php
-
 		$pregunta1 = $_POST['PreguntaUno'];
 		$pregunta2 = $_POST['PreguntaDos'];
 		$pregunta3 = $_POST['PreguntaTres'];
 		$pregunta4 = $_POST['PreguntaCuatro'];
-		$pregunta5 = $_POST['PreguntaCinco'];
-		
-		$mensaje = "";
-		
+		$pregunta5 = $_POST['PreguntaCinco'];		
+		$mensaje = "";	
 		$puntos = 0;
-
 	if($pregunta1 == "v")
 	{
 		$puntos = $puntos + 3;
@@ -30,7 +26,6 @@
 	{
 		$puntos = $puntos + 3;
 	}
-	
 	if(($puntos == 3) || ($puntos == 6))
 	{
 		$mensaje="Te falta ver más capítulos de los Simpson.";
@@ -41,7 +36,6 @@
 	{
 		$mensaje="¡Genial! Eres muy fanático de los Simpson.";
 	}
-	
 	echo "Resultado: $puntos puntos <br> $mensaje <br>";
 	echo "<b>Respuestas</b><br>";
 	echo "1.- Dios tiene 5 dedos <br> 2.- Bart es hermano de Lisa <br> 3.- Usa el nombre de sus familiares <br> 4.- MilHouse no tiene hermanos <br> 5.- Homero tiene 3 hijos";
